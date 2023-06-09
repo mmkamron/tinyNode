472c55454023cf2de3befc0c01d53b73029b8eb6
Kamron Mirsamatov <kronusk2@gmail.com>
2023-06-09 09:31:46 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const shortid = require('shortid');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+MongoClient.connect(mongoURI)
+  .then(client => {
+    console.log('Connected to MongoDB');
+    const db = client.db('urlshortener');
+    const urlsCollection = db.collection('urls');
+
+    // Route for shortening the URL
+    app.post('/shorten', (req, res) => {
+      const longUrl = req.body.longUrl;
+      const shortCode = shortid.generate();
+
+      urlsCollection.insertOne({ shortCode, longUrl })
+        .then(() => {
+          res.json({ shortUrl: `https://tinynode.jprq.live/${shortCode}` });
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        });
+    });
+
+    // Route for redirecting to the original URL
+    app.get('/:shortCode', (req, res) => {
+      const shortCode = req.params.shortCode;
+
+      urlsCollection.findOne({ shortCode })
+        .then(result => {
+          if (result) {
+            res.redirect(result.longUrl);
+          } else {
+            res.status(404).json({ error: 'Short URL not found' });
+          }
+        })
+        .catch(error => {
+          console.error(error);
+          res.status(500).json({ error: 'Internal server error' });
+        });
+    });
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
